8944946914cb8cc106b98ad649521eab2e1ef0c1
joyyan <joyyan927@gmail.com>
2014-05-23 10:01:46 +0800

    Thinking in Java

---
+++ ThinkingInJava/src/chapter1/HelloDate.java
@@ -0,0 +1,26 @@
+//: object/HelloDate.java
+package chapter1;
+
+/**
+ * The first Thingking ing Java example program display a string and today's
+ * date
+ * 
+ * @author yanyan
+ * @version 1.0
+ */
+public class HelloDate {
+	/**
+	 * Entrv point to class&app
+	 * 
+	 * @param args
+	 * @throws exceptiongs
+	 */
+
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("user.name"));
+		System.out.println(System.getProperty("java.library.path"));
+	} /*
+	 * Outpuy:(55% match) Hello.
+	 */// ~
+
+}
---
+++ ThinkingInJava/src/chapter12/MyException.java
@@ -0,0 +1,8 @@
+package chapter12;
+
+class MyException extends Exception {
+	public MyException() {}
+	public MyException(String msg) {
+		super(msg);
+	}
+}
